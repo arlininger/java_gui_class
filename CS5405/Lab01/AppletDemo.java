d6278c5dc77e85d372e3c39625cbe640f710aeb3
arlininger <arlininger@gmail.com>
2014-08-26 20:15:04 -0500

    Initial Draft
    Not working

---
+++ CS5405/Lab01/AppletDemo.java
@@ -0,0 +1,15 @@
+/*
+ * Adam Lininger
+ */
+
+import javax.swing.JApplet;
+import java.awt.Graphics;
+
+public class AppletDemo extends JApplet
+{
+	public void pain(Graphics g)
+	{
+		g.drawString("Adam Lininger",125,125);
+	}
+}
+
