c79de8d789fe1568a96346def7b86a50558aeab1
Adam Lininger <arlininger@gmail.com>
2014-11-01 19:18:29 -0500

    Working Bubble Sort

--- CS5405/project/source/BubbleSort.java
+++ CS5405/project/source/BubbleSort.java
@@ -6,12 +6,37 @@ package code;
 
 public class BubbleSort extends Sortable
 {
-	public void sort()
+	int i;
+	int j;
+	public void reset()
 	{
-
+		super.reset();
+		System.out.println("Bubble Reset");
+		i = 0;
+		j = 0;
 	}
 
-	public void stop()
+	public boolean sort_step()
 	{
+		if (i == size)
+		{
+			System.out.println("Complete");
+			return false;
+		}
+		if (array[j] > array[j+1])
+		{
+			//swap(array[i],array[i+1]);
+			array[j] ^= array[j+1];
+			array[j+1] ^= array[j];
+			array[j] ^= array[j+1];
+		}
+		j++;
+		if (j == size-1)
+		{
+			j = 0;
+			i++;
+		}
+		return true;
 	}
+
 }
--- CS5405/project/source/Sortable.java
+++ CS5405/project/source/Sortable.java
@@ -5,14 +5,21 @@
 package code;
 
 import java.awt.*;
+import java.awt.event.*;
 import java.util.*;
+import java.util.concurrent.*;
 import javax.swing.*;
 
-public abstract class Sortable extends JInternalFrame
+public abstract class Sortable extends JInternalFrame implements Runnable, ActionListener
 {
 	int[] array;
 	int size;
+	boolean running;
 	Random numberGenerator;
+	JButton playButton;
+	JButton resetButton;
+	JButton pauseButton;
+
 	public Sortable()
 	{
 		super("Bubble Sort",true,true,true,true);
@@ -27,20 +34,51 @@ public abstract class Sortable extends JInternalFrame
 	}
 	private void setup()
 	{
+		setLayout(new FlowLayout());
+		//JPanel buttons = new JPanel(new GridLayout(1,3));
+		//buttons.setMaximumSize(new Dimension(30,1000000)); 
+		playButton = new JButton("Play");
+		resetButton = new JButton("Reset");
+		pauseButton = new JButton("Pause");
+		playButton.addActionListener(this);
+		resetButton.addActionListener(this);
+		pauseButton.addActionListener(this);
+		add(playButton);
+		add(resetButton);
+		add(pauseButton);
+		//add(buttons);
 		numberGenerator = new Random();
-		setSize(400,400);
+		setSize(300,300);
 		setVisible(true);
 		toFront();
 		reset();
+		running = false;
+		Executor executor = Executors.newCachedThreadPool();
+		executor.execute(this);
 	}
 
-	public abstract void sort();
+	/**
+	 * Take one step in the sorting algorithm.
+	 * @return True if more steps are available.
+	 */
+	public abstract boolean sort_step();
 
-	public abstract void stop();
+	public void play()
+	{
+		running = true;
+		System.out.println("play");
+	}
+
+	public void pause()
+	{
+		running = false;
+		System.out.println("pause");
+	}
 
 	public void reset()
 	{
-		this.stop();
+		System.out.println("reset");
+		running = false;
 		array = new int[this.size];
 		for (int i = 0; i < this.size; i++)
 		{
@@ -61,7 +99,51 @@ public abstract class Sortable extends JInternalFrame
 
 		for (int i = 0; i < size; i++)
 		{
-			g.drawLine(10,10+i,10+array[i],10+i);
+			g.drawLine(10+i,180-array[i],10+i,180);
+		}
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		if (e.getSource() == playButton)
+		{
+			play();
+		} else if (e.getSource() == resetButton)
+		{
+			reset();
+		} else if (e.getSource() == pauseButton)
+		{
+			pause();
+		}
+		repaint();
+
+	}
+
+	public void run()
+	{
+		while (true)
+		{
+			if (running)
+			{
+				System.out.println("running");
+				if (sort_step() == false)
+				{
+					running = false;
+				}
+			}
+			repaint();
+			try
+			{
+				if (running) 
+				{
+					Thread.sleep(1); //Run quickly when running
+				}
+				else {
+					Thread.sleep(100); //Don't waste time when not running
+				}
+			} catch (InterruptedException ex)
+			{
+			}
 		}
 	}
 }
