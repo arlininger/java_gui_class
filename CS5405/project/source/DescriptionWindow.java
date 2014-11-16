9a95e9fb2c3a6a7ef66f2009c82baff0ee43e35f
Adam Lininger <arlininger@gmail.com>
2014-11-15 19:47:13 -0600

    Add remaining windows

---
+++ CS5405/project/source/DescriptionWindow.java
@@ -0,0 +1,38 @@
+/**
+ * @author Adam Lininger
+ */
+
+package code;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * The Help class displays a dialog box with a helpful description of the basic functionality
+ * of this program.
+ */
+public class DescriptionWindow extends JInternalFrame
+{
+	/**
+	 * Create a Help Window.
+	 * @param parent The parent window of the help window.
+	 */
+	public DescriptionWindow()
+	{
+		super("Help", true, true, true, true);
+		setLayout(new GridLayout(1,1));
+		setSize(300,300);
+		setVisible(true);
+		String helpContent = new StringBuilder()
+			.append("Description stuff\n")
+			.toString();
+		toFront();
+		//JPanel panel = new JPanel();
+		JTextArea textArea = new JTextArea(helpContent,30,30);
+		JScrollPane scrollArea = new JScrollPane(textArea);
+		add(scrollArea);
+	}
+}
+
+
